24fa8e60e793a005c4d1399116d4bc6c72e57b76
aymanouch <aymanouchedih1997@gmail.com>
2019-12-25 13:40:09 +0100

    romve folder

--- toghether-Company/js/selectlang.js
+++
@@ -1,8 +0,0 @@
-var myLink = document.getElementById("go-link");
-function checkLang(parm, lang) {
-    if (lang !== parm.value) {
-        window.location.href =  parm.value.toLowerCase();
-    }
-            
-  
-}
